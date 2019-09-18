cc27cc3398297271a49c6e2d490f72792164b193
yhx9527 <540230904@qq.com>
2019-09-18 22:30:29 +0800

    更新

---
+++ PAT-B1020-/main.cpp
@@ -0,0 +1,69 @@
+//
+//  main.cpp
+//  PAT-B1020-
+//
+//  Created by 雨含香 on 2019/9/18.
+//  Copyright © 2019 雨含香. All rights reserved.
+//
+//1020 月饼 (25 分)
+//月饼是中国人在中秋佳节时吃的一种传统食品，不同地区有许多不同风味的月饼。现给定所有种类月饼的库存量、总售价、以及市场的最大需求量，请你计算可以获得的最大收益是多少。
+//
+//注意：销售时允许取出一部分库存。样例给出的情形是这样的：假如我们有 3 种月饼，其库存量分别为 18、15、10 万吨，总售价分别为 75、72、45 亿元。如果市场的最大需求量只有 20 万吨，那么我们最大收益策略应该是卖出全部 15 万吨第 2 种月饼、以及 5 万吨第 3 种月饼，获得 72 + 45/2 = 94.5（亿元）。
+//
+//输入格式：
+//
+//每个输入包含一个测试用例。每个测试用例先给出一个不超过 1000 的正整数 N 表示月饼的种类数、以及不超过 500（以万吨为单位）的正整数 D 表示市场最大需求量。随后一行给出 N 个正数表示每种月饼的库存量（以万吨为单位）；最后一行给出 N 个正数表示每种月饼的总售价（以亿元为单位）。数字间以空格分隔。
+//
+//输出格式：
+//
+//对每组测试用例，在一行中输出最大收益，以亿元为单位并精确到小数点后 2 位。
+//
+//输入样例：
+//
+//3 20
+//18 15 10
+//75 72 45
+//输出样例：
+//
+//94.50
+
+
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
+using namespace std;
+struct mooncake{
+    double total_money;
+    double quantity;
+    double price;
+} mooncakes[1010];
+bool cmp(mooncake a, mooncake b){
+    return a.price > b.price;
+}
+
+int main(int argc, const char * argv[]) {
+    int num,weight;
+    scanf("%d %d", &num, &weight);
+    for(int i=0;i<num;i++){
+        scanf("%lf", &mooncakes[i].quantity);
+    }
+    for(int i=0;i<num;i++){
+        scanf("%lf", &mooncakes[i].total_money);
+        mooncakes[i].price = mooncakes[i].total_money/mooncakes[i].quantity;
+    }
+    sort(mooncakes, mooncakes+num, cmp);
+    double profit=0;
+    int index=0;
+    for(int i=0;i<num;i++){
+        if(weight>=mooncakes[i].quantity){
+            weight-=mooncakes[i].quantity;
+            profit+=mooncakes[i].total_money;
+        } else {
+            profit+=mooncakes[i].total_money*weight/mooncakes[i].quantity;
+            break;
+        }
+    }
+    
+    printf("%.2lf\n", profit);
+    return 0;
+}
---
+++ PAT-B1020/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  PAT-B1020
+//
+//  Created by 雨含香 on 2019/9/18.
+//  Copyright © 2019 雨含香. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
